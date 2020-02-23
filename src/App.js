5a17a96662679490655d60e987de522d487c8bc9
yogain19 <yogendrasaxena56@gmail.com>
2020-02-23 22:18:58 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
+  const [product, setProduct] = useState({
+    name: "React",
+    price: 10,
+    productBy: "fackbook"
+  });
+
+  const makePayment = token => {
+    console.log({ token });
+    const body = {
+      token,
+      product
+    };
+    const headers = { "Content-Type": "application/json" };
+
+    return fetch(`http://localhost:3009/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        console.log({ response });
+        const { status } = response;
+        console.log({ status });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +48,16 @@ function App() {
         >
           Learn React
         </a>
+        <StripeCheckout
+          stripeKey="YOUR-STRIPE-PUBLISH-KEY"
+          token={token => makePayment(token)}
+          name="Buy React"
+          amount={product.price * 100}
+        >
+          <button type="button" class="btn btn-primary">
+            Pay with Card
+          </button>
+        </StripeCheckout>
       </header>
     </div>
   );
